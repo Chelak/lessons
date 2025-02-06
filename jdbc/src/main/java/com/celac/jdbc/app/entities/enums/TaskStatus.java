d8fe247f57a9e8b33585c00e82a1cc6db458cd26
Serghei Celac <celaserg@gmail.com>
2025-02-06 08:39:41 +0200

    Add fw for processing of resultset (#5)

--- jdbc/src/main/java/com/celac/jdbc/app/entities/enums/TaskStatus.java
+++ jdbc/src/main/java/com/celac/jdbc/app/entities/enums/TaskStatus.java
@@ -1,9 +1,29 @@
 package com.celac.jdbc.app.entities.enums;
 
+import java.util.stream.Stream;
+
 public enum TaskStatus {
-  OPEN,
-  CLOSE,
-  PENDING,
-  BLOCKED,
-  IN_PROGRESS
+  OPEN("OPEN"),
+  CLOSE("CLOSE"),
+  PENDING("PENDING"),
+  BLOCKED("BLOCKED"),
+  IN_PROGRESS("IN_PROGRESS");
+
+  private final String value;
+
+  TaskStatus(String value) {
+    this.value = value;
+  }
+
+  public String getValue() {
+    return value;
+  }
+  public static TaskStatus fromValue(String value) {
+    for (TaskStatus t : TaskStatus.values()) {
+      if (t.value.equals(value)) {
+        return t;
+      }
+    }
+    return null;
+  }
 }
